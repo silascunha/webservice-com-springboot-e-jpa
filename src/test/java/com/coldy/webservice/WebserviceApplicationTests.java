88d7550819381515e8519ed31925093565d14153
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-27 18:26:12 -0300

    Criacao do projeto

---
+++ src/test/java/com/coldy/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coldy.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
