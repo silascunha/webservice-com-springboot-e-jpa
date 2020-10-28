7c33f8931c198eae9785777ea276361ac7097cb7
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-27 21:38:20 -0300

    User e UserResource

---
+++ src/main/java/com/coldy/webservice/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.coldy.webservice.resources;
+
+import com.coldy.webservice.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "991112234", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
