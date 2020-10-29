e720c096645c3e92cde45fdf0969c4aee0c454ff
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-28 23:48:28 -0300

    Classe Category e os seus resource, repository, e service part2

--- src/main/java/com/coldy/webservice/resources/CategoryResource.java
+++ src/main/java/com/coldy/webservice/resources/CategoryResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @Autowired
