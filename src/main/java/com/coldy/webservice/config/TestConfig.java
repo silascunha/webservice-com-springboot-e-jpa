89d75c13683af572a91680355e3212beeceb8b49
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-10-28 23:25:51 -0300

    OrderStatus enum na classe Order

--- src/main/java/com/coldy/webservice/config/TestConfig.java
+++ src/main/java/com/coldy/webservice/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.coldy.webservice.config;
 
 import com.coldy.webservice.entities.Order;
 import com.coldy.webservice.entities.User;
+import com.coldy.webservice.entities.enums.OrderStatus;
 import com.coldy.webservice.repositories.OrderRepository;
 import com.coldy.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Silas Cunha", "silas@gmail.com", "988888888", "12345");
         User u2 = new User(null, "Ana Paula", "anap@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2020-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2020-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2020-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2020-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2020-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2020-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
