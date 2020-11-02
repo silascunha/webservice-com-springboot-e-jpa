0557767ccd61275f49603e154d652b8fc8c50300
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-11-02 17:35:18 -0300

    Associação oneToMany com Product para OrderItem

--- src/main/java/com/coldy/webservice/entities/Product.java
+++ src/main/java/com/coldy/webservice/entities/Product.java
@@ -1,5 +1,7 @@
 package com.coldy.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -23,6 +25,9 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {}
 
@@ -76,6 +81,14 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        items.forEach(x -> set.add(x.getOrder()));
+
+        return set;
     }
 
     @Override
