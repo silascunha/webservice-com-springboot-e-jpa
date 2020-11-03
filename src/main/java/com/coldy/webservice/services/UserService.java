f26cd99d736205457dd8eeb703b3338cd755a2a9
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-11-03 16:21:02 -0300

    Tratamento de exceções delete

--- src/main/java/com/coldy/webservice/services/UserService.java
+++ src/main/java/com/coldy/webservice/services/UserService.java
@@ -2,8 +2,11 @@ package com.coldy.webservice.services;
 
 import com.coldy.webservice.entities.User;
 import com.coldy.webservice.repositories.UserRepository;
+import com.coldy.webservice.services.exceptions.DatabaseException;
 import com.coldy.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
