dfea4ac53e1cc64477cf668f891fafbf8c2bd849
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2020-11-03 16:08:18 -0300

    Tratamento de exceções findById

--- src/main/java/com/coldy/webservice/services/UserService.java
+++ src/main/java/com/coldy/webservice/services/UserService.java
@@ -2,6 +2,7 @@ package com.coldy.webservice.services;
 
 import com.coldy.webservice.entities.User;
 import com.coldy.webservice.repositories.UserRepository;
+import com.coldy.webservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
---
+++ src/main/java/com/coldy/webservice/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.coldy.webservice.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id: " + id);
+    }
+
+}
